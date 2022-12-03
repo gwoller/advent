15baa9b93f4cf878a8ac5c754ee1692e60419387
gwoller <gabriel.woller@gmail.com>
2022-12-02 21:14:35 -0500

    Day 2 Complete

--- 2022/Dec2_22.m
+++ 2022/Dec2_22.m
@@ -3,4 +3,90 @@ clear
 close all
 tic
 
-A = input('Input: ',"s");
+[~, guide] = xlsread('Dec2Input.xlsx');
+
+guide = split(guide);
+score = zeros(length(guide),1);
+
+for i = 1:length(guide)
+    if isequal(guide(i,2), {'X'})
+        if isequal(guide(i,1), {'A'})
+            score(i) = 4;
+        end
+        if isequal(guide(i,1), {'B'})
+            score(i) = 1;
+        end
+        if isequal(guide(i,1), {'C'})
+            score(i) = 7;
+        end
+    end
+
+    if isequal(guide(i,2), {'Y'})
+        if isequal(guide(i,1), {'A'})
+            score(i) = 8;
+        end
+        if isequal(guide(i,1), {'B'})
+            score(i) = 5;
+        end
+        if isequal(guide(i,1), {'C'})
+            score(i) = 2;
+        end
+    end
+
+    if isequal(guide(i,2), {'Z'})
+        if isequal(guide(i,1), {'A'})
+            score(i) = 3;
+        end
+        if isequal(guide(i,1), {'B'})
+            score(i) = 9;
+        end
+        if isequal(guide(i,1), {'C'})
+            score(i) = 6;
+        end
+    end
+end
+
+total = sum(score(:,1));
+
+score_2 = zeros(length(guide(:,1)));
+
+for i = 1:length(guide)
+    if isequal(guide(i,2), {'X'})
+        if isequal(guide(i,1), {'A'})
+            score_2(i) = 3;
+        end
+        if isequal(guide(i,1), {'B'})
+            score_2(i) = 1;
+        end
+        if isequal(guide(i,1), {'C'})
+            score_2(i) = 2;
+        end
+    end
+
+    if isequal(guide(i,2), {'Y'})
+        if isequal(guide(i,1), {'A'})
+            score_2(i) = 4;
+        end
+        if isequal(guide(i,1), {'B'})
+            score_2(i) = 5;
+        end
+        if isequal(guide(i,1), {'C'})
+            score_2(i) = 6;
+        end
+    end
+
+    if isequal(guide(i,2), {'Z'})
+        if isequal(guide(i,1), {'A'})
+            score_2(i) = 8;
+        end
+        if isequal(guide(i,1), {'B'})
+            score_2(i) = 9;
+        end
+        if isequal(guide(i,1), {'C'})
+            score_2(i) = 7;
+        end
+    end
+end
+
+final = sum(score_2(:,1));
+toc
