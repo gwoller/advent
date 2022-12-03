ac98abb8ff072b6732f95f5e0f26c16139a13f5e
gwoller <gabriel.woller@gmail.com>
2022-12-02 20:46:12 -0500

    Day 2

--- 2022/Dec2_22.m
+++ 2022/Dec2_22.m
@@ -3,4 +3,20 @@ clear
 close all
 tic
 
-A = input('Input: ',"s");
+[~, guide] = xlsread('Dec2Input.xlsx');
+
+guide = split(guide);
+
+for i = 1:length(guide)
+    if guide(i,1) == 'A'
+        if guide(i,2) == 'X'
+            guide(i,3) = 4;
+        end
+        if guide(i,2) == 'Y'
+            guide(i,3) = 8;
+        end
+        if guide(i,2) == 'Z'
+            guide(i,3) = 3;
+        end
+    end
+end
