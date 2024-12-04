20ea554d9bfdb2b8d6fda93b8ea933d78c997621
gwoller <52172677+gwoller@users.noreply.github.com>
2024-12-03 23:08:18 -0500

    Rewrote for brevity

--- 2024/Day3.m
+++ 2024/Day3.m
@@ -3,33 +3,28 @@ clear variables
 
 data = readlines('Day3Input.txt');
 
+%% Part 1
 str = append(data(1),data(2),data(3),data(4),data(5),data(6));
 
-expression = 'mul\(\d*,\d*\)';
-str = regexp(str,expression,'match');
-
-extractedText = extractBetween(str,'mul(',')')';
-
-newStr = split(extractedText,',');
-nums = str2double(newStr);
-
-tot = sum(nums(:,1).*nums(:,2))
+tot = funcDay3(str)
 
 %% Part 2
-
-clearvars -except data
-
-str = append(data(1),data(2),data(3),data(4),data(5),data(6));
 
 newStr = replaceBetween(str,"don't()","do()"," ",'Boundaries','inclusive');
 newStr = extractBefore(newStr,"don't()");
 
-expression = 'mul\(\d*,\d*\)';
-extStr = regexp(newStr,expression,'match');
+tot = funcDay3(newStr)
 
-extractedText = extractBetween(extStr,'mul(',')')';
+%% Function
+function tot = funcDay3(str)
+    
+    expression = 'mul\(\d*,\d*\)';
+    str = regexp(str,expression,'match');
+    
+    extractedText = extractBetween(str,'mul(',')')';
+    
+    nums = str2double(split(extractedText,','));
+    
+    tot = sum(nums(:,1).*nums(:,2));
 
-newStr = split(extractedText,',');
-nums = str2double(newStr);
-
-tot = sum(nums(:,1).*nums(:,2))
+end
